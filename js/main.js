5fa3e37c63fd8dcc9bb8afd9803e15002eab96e3
Abd <abd3habib@gmail.com>
2022-08-24 15:31:41 +0300

    Memory Game Using HTML & CSS & JS

---
+++ js/main.js
@@ -0,0 +1,88 @@
+document.querySelector(".control-button span").onclick = function () {
+  let yourName = prompt("Whats Your Name?");
+
+  if (yourName == null || yourName == "") {
+    document.querySelector(".name span").innerHTML = "Unknown";
+  } else {
+    document.querySelector(".name span").innerHTML = yourName;
+  }
+
+  document.querySelector(".control-button").remove();
+};
+
+let duration = 1000;
+
+let blocksContainer = document.querySelector(".memory-game-blocks");
+
+let blocks = Array.from(blocksContainer.children);
+
+let orderRange = [...Array(blocks.length).keys()];
+
+orderRange = shuffle(orderRange);
+
+console.log(shuffle(orderRange));
+
+blocks.forEach((block, index) => {
+  block.style.order = orderRange[index];
+
+  block.addEventListener("click", function () {
+    flipBlock(block);
+  });
+});
+
+function flipBlock(selectedBlock) {
+  selectedBlock.classList.add("is-flipped");
+
+  let allFlippedBlocks = blocks.filter((flippedBlock) =>
+    flippedBlock.classList.contains("is-flipped")
+  );
+
+  if (allFlippedBlocks.length === 2) {
+    stopClicking();
+
+    checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1]);
+  }
+}
+
+function stopClicking() {
+  blocksContainer.classList.add("no-clicking");
+
+  setTimeout(function () {
+    blocksContainer.classList.remove("no-clicking");
+  }, duration);
+}
+
+function checkMatchedBlocks(firstBlock, secondBlock) {
+  let triesElement = document.querySelector(".tries span");
+
+  if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
+    firstBlock.classList.remove("is-flipped");
+    secondBlock.classList.remove("is-flipped");
+
+    firstBlock.classList.add("has-match");
+    secondBlock.classList.add("has-match");
+    document.getElementById("success").play();
+  } else {
+    triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
+
+    setInterval(function () {
+      firstBlock.classList.remove("is-flipped");
+      secondBlock.classList.remove("is-flipped");
+    }, duration);
+    document.getElementById("fail").play();
+  }
+}
+
+function shuffle(array) {
+  let current = array.length,
+    temp,
+    random;
+
+  while (current > 0) {
+    random = Math.floor(Math.random() * current);
+    current--;
+
+    [array[current], array[random]] = [array[random], array[current]];
+  }
+  return array;
+}
